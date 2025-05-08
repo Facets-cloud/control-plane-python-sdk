f75a9708a58fdeadf5f7c27fdccc8029e2f0f74d
Sanmesh Kakade <sanmesh.kakade@facets.cloud>
2025-05-08 16:43:31 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -14,26 +14,28 @@ from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "swagger-client"
 VERSION = "1.1.0"
-# To install the library, run the following
-#
-# python setup.py install
-#
-# prerequisite: setuptools
-# http://pypi.python.org/pypi/setuptools
-
 REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name=NAME,
     version=VERSION,
     description="Api Documentation",
-    author_email="",
-    url="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Anuj Hydrabadi",
+    author_email="anujhydrabadi@facets.cloud",
+    url="https://github.com/Facets-cloud/control-plane-python-sdk",
     keywords=["Swagger", "Api Documentation"],
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
-    long_description="""\
-    Api Documentation  # noqa: E501
-    """
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
